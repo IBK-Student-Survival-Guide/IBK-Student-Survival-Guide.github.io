d9433e4b27892214c647663e86c512bb2a67c29e
pruje839 <jeffprumbaum@hotmail.com>
2025-06-18 14:57:35 +0200

    map2.js und map3.js erstellt

--- freizeittirol/map2.js
+++ freizeittirol/map2.js
@@ -1,4 +1,4 @@
-//Karte 1 leicht 
+//Karte 2 mittel
 
 // Innsbruck
 let ibk = {
@@ -6,8 +6,8 @@ let ibk = {
     lng: 11.392778
 };
 
-//Karte 1 initialisieren
-let map1 = L.map("map1").setView([ibk.lat, ibk.lng], 9);
+//Karte 2 initialisieren
+let map2 = L.map("map2").setView([ibk.lat, ibk.lng], 9);
 
 // WMTS Hintergrundlayer der eGrundkarte Tirol
 let eGrundkarteTirol = {
@@ -29,46 +29,45 @@ L.control.layers({
     "eGrundkarte Tirol Sommer": L.layerGroup([
         eGrundkarteTirol.sommer,
         eGrundkarteTirol.nomenklatur
-    ]).addTo(map1),
+    ]).addTo(map2),
     "eGrundkarte Tirol Orthofoto": L.layerGroup([
         eGrundkarteTirol.ortho,
         eGrundkarteTirol.nomenklatur,
     ]),
     "OpenStreetMap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
     "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
-}).addTo(map1);
+}).addTo(map2);
 
 // Maßstab
 L.control.scale({
     imperial: false,
-}).addTo(map1);
+}).addTo(map2);
 
-//Elevation Map 1
+//Elevation Map 2
 
 const controlElevation = L.control.elevation({
-    theme: "leicht",
+    theme: "mittel",
     time: false,
-    elevationDiv: "#profile1", 
+    elevationDiv: "#profile2", 
     height: 300, 
     slope: true, 
  }).addTo(map1);
- controlElevation.load("gpx/leicht.gpx");
+ controlElevation.load("gpx/mittel.gpx");
 
- //Minimap Map 1
+ //Minimap Map 2
 
 var gkTirol = new L.TileLayer("https://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png"); 
 var miniMap = new L.Control.MiniMap(gkTirol,{
     toggleDisplay: true
-}).addTo(map1);
+}).addTo(map2);
 
 //Fullscrean 
-map1.addControl(new L.Control.Fullscreen()); 
+map2.addControl(new L.Control.Fullscreen()); 
 
 //Locate controle
     var lc = L.control
         .locate({
             position: "topright",
         })
-        .addTo(map1);
-
+        .addTo(map2);
 
